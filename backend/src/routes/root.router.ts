96df56435626f655c24061d6ee7f1aaef96d6e64
Aditya Maheshwari <adityamahesh@umass.edu>
2024-02-20 22:38:50 -0800

    Adding routes

--- backend/src/routes/root.router.ts
+++ backend/src/routes/root.router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import userRouter from "./user.routes";
 
 const router = Router();
 
@@ -6,5 +7,8 @@ const router = Router();
 router.get("/", (req, res) => {
   res.status(200).send({ status: "ok" });
 });
+
+// User routes
+router.use("/user", userRouter);
 
 export default router;
